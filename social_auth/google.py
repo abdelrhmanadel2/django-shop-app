620ab8866a9c5ac86a044f99f63aae5bf1c9a4ab
Pritam Tajpuriya <pritamtajpuriya@gmail.com>
2021-07-07 08:26:56 +0545

    google client id added to verifications

--- social_auth/google.py
+++ social_auth/google.py
@@ -13,10 +13,22 @@ class Google:
         try:
             # idinfo=id_token.verify_oauth2_token(auth_token)
             idinfo = id_token.verify_oauth2_token(
-                auth_token, requests.Request(),)
+                auth_token, requests.Request())
 
-            if 'https://accounts.google.com' in idinfo['iss']:
+            if idinfo['iss'] in ['https://accounts.google.com',]:
                 return idinfo
+            
+
+               # Or, if multiple clients access the backend server:
+    # idinfo = id_token.verify_oauth2_token(token, requests.Request())
+    # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
+    #     raise ValueError('Could not verify audience.')
+
+    # If auth request is from a G Suite domain:
+    # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+    #     raise ValueError('Wrong hosted domain.')
+
+    # ID token is valid. Get the user's Google Account ID from the decoded token.
 
         except:
             return "The token is either invalid or has expired"
--- social_auth/serializers.py
+++ social_auth/serializers.py
@@ -15,7 +15,7 @@ class GoogleSocialAuthSerializer(serializers.Serializer):
                 'The token is invalid or expired. Please login again.'
             )
 
-        # if user_data['aud'] != '1022319866638-olgqu540jut15das6hmnjriiaenidjsc.apps.googleusercontent.com':
+        if user_data['aud'] != '1022319866638-olgqu540jut15das6hmnjriiaenidjsc.apps.googleusercontent.com':
         # os.environ.get('GOOGLE_CLIENT_ID'):
 
             raise AuthenticationFailed('oops, who are you?')
