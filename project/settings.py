ca35a5d3dda4be28eede7980568e9d100737b95a
Pritam Tajpuriya <pritamtajpuriya@gmail.com>
2021-07-15 08:26:43 +0545

    sc

--- project/settings.py
+++ project/settings.py
@@ -230,5 +230,5 @@ EMAIL_USE_TLS= True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='testdjango102@gmail.com'
-EMAIL_HOST_PASSWORD='testDjango@102'
+EMAIL_HOST_PASSWORD='test@1234G'
 
